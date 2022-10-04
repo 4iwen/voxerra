cc546067cb4cc92590d26a2f214204db985e00fa
Tomáš Vik <GepardCZE@post.cz>
2022-10-04 12:55:33 +0200

    a

--- src/WeaponClass.cpp
+++ src/WeaponClass.cpp
@@ -30,13 +30,13 @@ float WeaponClass::CalculateDamage(float armor) {
             return damage+(sharpness + (pow(sharpness,1.5) / (pow(armor, 0.125))));
             break;
         case Hammer:
-            return damage+(heaviness + (pow(heaviness,1.2) * (pow(armor, 0.15))));
+            return damage+(heaviness + (pow(heaviness,1.2) * (pow(armor, 0.2))));
             break;
         case Fists:
             return damage+(heaviness + (pow(heaviness,1.2) * (pow(armor, 0.15))));
             break;
         case Staff:
-            return damage+(heaviness + (pow(heaviness,1.2) * (pow(armor, 0.15))));
+            return damage+(heaviness + (pow(heaviness,1.2) * (pow(armor, 0.175))));
             break;
         default:
             return damage+((sharpness + (pow(sharpness,1.5) / (pow(armor, 0.125)))+(heaviness + (pow(heaviness,1.2) * (pow(armor, 0.15))))))/1.75;
